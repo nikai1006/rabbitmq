231394abd6b541a9f88d171b19f48efad0edc512
nikai <keni1006@163.com>
2019-07-22 00:59:48 +0800

    config

---
+++ consumer/src/main/java/com/nikai/demo/rabbitmq/consumer/config/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package com.nikai.demo.rabbitmq.consumer.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * rabbitmq com.nikai.demo.rabbitmq.consumer.config
+ *
+ * @author: nikai
+ * @Description:
+ * @Date: Create in 0:59 2019/7/22
+ * @Modified By:
+ */
+@Configuration
+public class RabbitMQConfig {
+
+}
---
+++ producer/src/main/java/com/nikai/demo/rabbitmq/producer/config/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package com.nikai.demo.rabbitmq.producer.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * rabbitmq com.nikai.demo.rabbitmq.producer.config
+ *
+ * @author: nikai
+ * @Description:
+ * @Date: Create in 0:58 2019/7/22
+ * @Modified By:
+ */
+@Configuration
+public class RabbitMQConfig {
+
+}
