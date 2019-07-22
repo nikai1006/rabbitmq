ff12af460e6c7edc427a24d647b835ff3b10cdeb
nikai <854324946@qq.com>
2019-07-22 09:47:48 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RabbitMQ demo
+
+
+#### 简介
+
+1.  支持AMQP协议
+2. 支持[MQTT](https://www.runoob.com/w3cnote/mqtt-intro.html)协议
--- consumer/src/main/java/com/nikai/demo/rabbitmq/consumer/RabbitmqConsumerApplication.java
+++ consumer/src/main/java/com/nikai/demo/rabbitmq/consumer/RabbitmqConsumerApplication.java
@@ -1,9 +1,11 @@
 package com.nikai.demo.rabbitmq.consumer;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class RabbitmqConsumerApplication {
 
 	public static void main(String[] args) {
